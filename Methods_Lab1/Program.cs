df15830565921cffd79b07d04dc5d37cd6ba9630
ShinkiMinkiNon <motis2004star@gmail.com>
2024-02-10 09:07:05 +0300

    I hope that will be last commit

--- Methods_Lab1/Program.cs
+++ Methods_Lab1/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Первая строчка стишка
 // <3 The second line <3
 // Eto tretya stroka
-// <3 And that's the fourth line :_)
+// <3 And that's the fourth line :_). Double sad line :_(
 // Теперь это я вроде должен(Никитос Носырев)
